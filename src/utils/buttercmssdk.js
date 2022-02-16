1d6ef19f1409c5593beec38f007a666b889996cf
Prokop Simek <prokop@dxheroes.io>
2022-02-16 22:01:05 +0100

    fix: env to boolean

--- src/utils/buttercmssdk.js
+++ src/utils/buttercmssdk.js
@@ -3,9 +3,10 @@ import Butter from 'buttercms';
 export let butterCMS;
 
 try {
-  butterCMS = Butter(process.env.REACT_APP_BUTTER_CMS_API_KEY, process.env.REACT_APP_BUTTER_CMS_PREVIEW);
+  butterCMS = Butter(process.env.REACT_APP_BUTTER_CMS_API_KEY, !process.env.REACT_APP_BUTTER_CMS_PREVIEW);
 } catch (error) {
   console.error(error)
+  throw error
 }
 
 
