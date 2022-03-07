8c46c2e8e7b6746860506c2d14dbdd593d8c4a2c
Prokop Simek <prokop@dxheroes.io>
2022-03-07 20:21:19 +0100

    fix: preview mode true/false to API request

--- src/utils/buttercmssdk.js
+++ src/utils/buttercmssdk.js
@@ -3,7 +3,9 @@ import Butter from 'buttercms';
 export let butterCMS;
 
 try {
-  butterCMS = Butter(process.env.REACT_APP_BUTTER_CMS_API_KEY, !process.env.REACT_APP_BUTTER_CMS_PREVIEW);
+  const butterCmsPreview = !(process.env.REACT_APP_BUTTER_CMS_PREVIEW === "false" || process.env.REACT_APP_BUTTER_CMS_PREVIEW === "0")
+  
+  butterCMS = Butter(process.env.REACT_APP_BUTTER_CMS_API_KEY, butterCmsPreview);
 } catch (error) {
   console.error(error)
 }
